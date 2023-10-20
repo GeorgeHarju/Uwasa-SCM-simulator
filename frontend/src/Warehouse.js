19e3f41adc6a9d3d4eabdc34364df8763c1ac839
Riku Ahonen <ahonen.riku@hotmail.com>
2023-10-20 17:26:01 +0300

    GET request to fetch data

--- frontend/src/Warehouse.js
+++ frontend/src/Warehouse.js
@@ -29,25 +29,47 @@ function Warehouse() {
             },
             body: jsonData,
         })
+        // logataan tiedot, jotka lähetettiin
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             console.log(typeof(data));
+            console.log(data);
         })
         .catch((error) => console.error('Error:', error));
     }
 
+    const handleGet = () => {
+        console.log('lets get');
+        fetch('http://localhost:3001/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        // parsitaan json, data-objekti sisältää tiedot
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(typeof(data));
+            console.log(data);
+        })
+    }
+
+    // formin avulla lähetetään post request eli lisätään uusi warehouse
+    // napin avulla lähetetään get request eli haetaan kaikki warehouset
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="id" placeholder="Warehouse id" value={warehouseData.id} onChange={handleChange} />
-            <input type="text" name="name" placeholder="Warehouse name" value={warehouseData.name} onChange={handleChange} />
-            <input type="text" name="latitude" placeholder="Latitude" value={warehouseData.latitude} onChange={handleChange} />
-            <input type="text" name="longitude" placeholder="Longitude" value={warehouseData.longitude} onChange={handleChange} />
-            <input type="text" name="processing_cost" placeholder="Processing cost" value={warehouseData.processing_cost} onChange={handleChange} />
-            <input type="text" name="max_hr_cap" placeholder="Max hr cap" value={warehouseData.max_hr_cap} onChange={handleChange} />
-            <input type="text" name="sla" placeholder="SLA" value={warehouseData.sla} onChange={handleChange} />
-            <input type="submit" value="Add warehouse" />
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="id" placeholder="Warehouse id" value={warehouseData.id} onChange={handleChange} />
+                <input type="text" name="name" placeholder="Warehouse name" value={warehouseData.name} onChange={handleChange} />
+                <input type="text" name="latitude" placeholder="Latitude" value={warehouseData.latitude} onChange={handleChange} />
+                <input type="text" name="longitude" placeholder="Longitude" value={warehouseData.longitude} onChange={handleChange} />
+                <input type="text" name="processing_cost" placeholder="Processing cost" value={warehouseData.processing_cost} onChange={handleChange} />
+                <input type="text" name="max_hr_cap" placeholder="Max hr cap" value={warehouseData.max_hr_cap} onChange={handleChange} />
+                <input type="text" name="sla" placeholder="SLA" value={warehouseData.sla} onChange={handleChange} />
+                <input type="submit" value="Add warehouse" />
+            </form>
+            <button onClick={handleGet}>GET</button>
+        </div>
     );
 }
 
